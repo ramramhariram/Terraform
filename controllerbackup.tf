e231ff6f238c71e985b6d9a68e48b7c42395fee6
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2020-06-15 15:04:51 -0700

    controller backup does not work...removing

--- controllerbackup.tf
+++
@@ -1,9 +0,0 @@
-resource "aviatrix_controller_config" "controller_backup_config" {
-  backup_configuration       = true
-  backup_cloud_type          = 1
-  backup_account_name        = "hrs-aviatrix"
-  sg_management_account_name = "hrs-aviatrix"
-  backup_bucket_name         = var.s3bucket
-  multiple_backups           = true
-  security_group_management  = true
-}
